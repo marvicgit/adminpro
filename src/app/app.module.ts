4159a251fb5ca98926505d63904e52eb61a64f7b
marvic <martinhuacho@hotmail.com>
2019-10-14 22:10:22 -0500

    enlanzado registrar usario backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,8 @@ import { ServiceModule } from './services/shared/service.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     PagesModule,
     ServiceModule
   ],
